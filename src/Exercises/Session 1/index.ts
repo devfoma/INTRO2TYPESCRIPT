dd6a91e0b99b9a42d7176898d5e37f71ed84d572
Ati-Devv <aatiloluwa@gmail.com>
2025-11-16 12:26:53 -0200

    chore: My GIDA Skillup Typescript class Activity

--- src/Exercises/Session 1/index.ts
+++ src/Exercises/Session 1/index.ts
@@ -1,1 +1,8 @@
 // Hehe yes it's empty you have to fill it with your solutions
+
+function printId(id: string | number) {
+                console.log("User ID is:", id);
+}
+
+printId(101);
+printId("abc123");
