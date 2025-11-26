2e3fb5d2382123a766cc1f84766e13d7683b7586
wisdomokereke <159027943+wisdomokereke@users.noreply.github.com>
2025-11-26 03:49:19 +0100

    started working on class and interfaces

---
+++ src/Exercises/Session2/class.ts
@@ -0,0 +1,79 @@
+/*
+5. Classes and Access Modifiers
+
+### Scenario:
+Create a Car class with:
+
+brand (public)
+
+model (public)
+
+engineNumber (private)
+
+year (protected)
+
+Add a constructor to set these values and a method displayInfo() that logs only public properties.
+
+### Instructions:
+
+Use access modifiers properly.
+
+Try to access private and protected properties outside the class and observe what happens.
+
+Create a subclass ElectricCar that extends Car and adds a batteryLife property.
+*/
+
+// Classes
+class Coder {
+    // name: string;
+    // music: string;
+    // age: number;
+    // lang: string;
+
+    constructor(
+        public name: string,
+        public music: string,
+        private age: number,
+        protected lang: string
+    ) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+
+    public getLang(): string {
+        return `${this.name} programs in ${this.lang}`;
+    }
+}
+
+const Emma = new Coder("Emma", "Afrobeat", 20, "Typescript");
+
+console.log(Emma);
+console.log(Emma.getLang());
+
+// Creating a subclass of Coder
+class webDev extends Coder{
+    constructor(
+        public computer: string,
+        private github: string,
+        name: string,
+        music: string,
+        age: number,
+        lang: string
+    ) {
+        super(name, music, age, lang);
+        this.computer = computer;
+        this.github = github;
+    }
+
+    public getGitHub() {
+        return `The Github username of ${this.name} is ${this.github}`;
+    }
+}
+
+// Identifying Access Modifiers Usage
+console.log(Emma.name);
+console.log(Emma.music);
+// console.log(Emma.age);
+// console.log(Emma.lang);
---
+++ src/Exercises/Session2/interface.ts
@@ -0,0 +1,86 @@
+interface Person {
+    name: string,
+    age: number
+}
+
+interface Employee extends Person {
+    department: string,
+    salary: number
+}
+
+let person: Person = {
+    name: "Wisdom Okereke",
+    age: 59_000_999_999_000_000_000
+}
+
+/*
+4. Interfaces and Interface Inheritance
+
+### Scenario:
+Create an interface Person with name and age.
+Then extend it to Employee, adding department and salary.
+Create an object that follows the Employee structure and log its properties.
+
+### Instructions:
+
+Use extends for inheritance.
+
+Type the object properly, do not use any.
+
+Use template literals to display the details.
+*/
+
+// interface User {
+//     name: string,
+//     email: string,
+//     phone: string
+// }
+
+// interface Account {
+//     accNo: string,
+//     balance: number
+// }
+
+// let user: User = {
+//     name: "Jackson",
+//     email: "jackofalltrade@gmail.bia",
+//     phone: "09000000000"
+// }
+
+// let account: Account = {
+//     accNo: "AC0987",
+//     balance: 70000
+// }
+
+// type Customer = User & Account;
+// let customer: Customer = {...user, ...account};
+// console.log(customer); 
+
+// New Work
+// interface Employee {
+//     readonly id: number;
+//     name: string,
+//     salary: number
+// }
+
+// let employee: Employee = {
+//     id : 123,
+//     name: "John Obot",
+//     salary: 500_000
+// }
+
+// interface Manager extends Employee {
+//     department: string,
+//     officeNo: number;
+// }
+
+// let manager: Manager = {
+//     id: 2,
+//     name: "Joseph Joseph Sunday",
+//     salary: 3_000_000,
+//     department: "Sales",
+//     officeNo: 500
+// }
+
+// console.log(`Employee (name: ${employee.name}, Salary: ${employee.salary})`);
+// console.log(`Manager: (Name | ${manager.name}, Salary | ${manager.salary}, department | ${manager.department})`);
