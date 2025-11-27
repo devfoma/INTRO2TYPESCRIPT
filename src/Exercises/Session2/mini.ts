d5d4d8bf57e3fe60b76815dd9ab5f1b9e27148e9
wisdomokereke <159027943+wisdomokereke@users.noreply.github.com>
2025-11-27 09:25:47 +0100

    Still working on the mini project

---
+++ src/Exercises/Session2/mini.ts
@@ -0,0 +1,58 @@
+//Student Management System
+/*
+Step 6: How to Organize
+
+Start with the enum → this is simple and independent.
+
+Define the interface → then your class can use it as a type.
+
+Write the class → define private array, methods.
+
+Test your class → create some Student objects and add them to the report.
+*/
+
+enum Gradelevel {
+    Sophomore,
+    Freshman,
+    Junior,
+    Senior
+}
+
+j
+
+/*
+6. Mini Project: Student Management System
+
+### Scenario:
+Build a small student management system using what you have learned.
+
+### Requirements:
+
+Create an enum GradeLevel with: Freshman, Sophomore, Junior, Senior.
+
+Create an interface Student with:
+
+name
+
+age
+
+gradeLevel (enum)
+
+subjects (tuple of [subjectName, score])
+
+Create a class Report that:
+
+Has a private array of students.
+
+Has a method addStudent() to add a new student.
+
+Has a method printReport() to display all student information.
+
+### Instructions:
+
+Use all the concepts from this week.
+
+Focus on correct typing and structure.
+
+Keep your code organized and readable.
+*/
